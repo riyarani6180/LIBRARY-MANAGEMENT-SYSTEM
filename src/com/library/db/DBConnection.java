d3747c857486f4c2fc6910a056181f701ad4e21c
FatCat <ganapatibagh1@gmail.com>
2026-04-25 12:50:43 +0530

    adding db connection and a different project structure

---
+++ src/com/library/db/DBConnection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String HOST = "";
+    private static final String PORT = "";
+    private static final String DB_NAME = "";
+    private static final String USER = "";
+    private static final String PASS = "";
+
+    private static final String URL = "jdbc:postgresql://" + HOST + ":" + PORT + "/" + DB_NAME;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("PostgreSQL driver not found", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
